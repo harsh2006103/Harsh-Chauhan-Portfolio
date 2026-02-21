349d05189de55466c25b643260e4cfa0d54f60f6
Harsh Chauhan <harshchetanchauhan@gmail.com>
2026-02-21 15:10:48 +0530

    change the profile

--- src/components/homepage/article.jsx
+++ src/components/homepage/article.jsx
@@ -11,26 +11,7 @@ const Article = (props) => {
 
 	return (
 		<React.Fragment>
-			<div className="homepage-article">
-				<div className="homepage-article-content">
-					<div className="homepage-article-date">
-						|&nbsp;&nbsp;&nbsp;{date}
-					</div>
-					<div className="homepage-article-title">{title}</div>
-					<div className="homepage-article-description">
-						{description}
-					</div>
-					<div className="homepage-article-link">
-						<Link to={link}>
-							Read article{" "}
-							<FontAwesomeIcon
-								style={{ fontSize: "10px" }}
-								icon={faChevronRight}
-							/>
-						</Link>
-					</div>
-				</div>
-			</div>
+			
 		</React.Fragment>
 	);
 };
